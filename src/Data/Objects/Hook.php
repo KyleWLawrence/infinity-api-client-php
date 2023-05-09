6f696d39913b939a1e6c8e2181d8b93b7159d3b3
Kyle Lawrence <kyle@designanddevelop.io>
2023-05-09 12:08:20 -0500

    first commit

--- src/Data/Objects/Hook.php
+++ src/Data/Objects/Hook.php
@@ -4,13 +4,27 @@ namespace KyleWLawrence\Infinity\Data\Objects;
 
 class Hook extends ObjectBase
 {
-    public array $events;
+    protected array $events;
 
-    public string $url;
+    protected string $url;
+
+    protected ?int $user_id;
+
+    protected ?string $secret;
+
+    protected ?array $logs;
 
     protected array $required = ['url', 'events'];
 
     protected array $updateVars = ['url', 'events'];
 
     protected ?string $object = 'hook';
+
+    public function getMostRecentLog()
+    {
+        $last = $this->logs;
+        $last = end($last);
+
+        return $last;
+    }
 }
